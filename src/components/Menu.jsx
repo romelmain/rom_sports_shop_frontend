3ce4f0196325bac0e378312290c6b03ffcfb9160
romelmain <romelherreratgk@gmail.com>
2024-11-30 23:14:00 -0400

    Ajustes en el Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
 import "./Menu.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 import { Link } from "react-router-dom";
 import ModalLogout from "./modalLogout";
 import Button from "react-bootstrap/Button";
@@ -8,30 +12,20 @@ import { ModalLogoutContext } from "../contexts/modallogout.context";
 
 function Menu() {
   const { show, setShow, setModal } = useContext(ModalLogoutContext);
+
   return (
     <>
-      <nav>
-        <label className="logo">Rom Sport Shop</label>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/products">Products</Link>
-          </li>
-          <li>
-            <Link to="/settings">Settings</Link>
-          </li>
-          <li>
-            <Link onClick={setModal} to="#">
-              Logout
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <div>
-        <ModalLogout></ModalLogout>
-      </div>
+      <Navbar className="transparentNavBar" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">Rom Sport Shop</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/products">Products</Nav.Link>
+            <Nav.Link href="/settings">Settings</Nav.Link>
+            <Nav.Link onClick={setModal}>Logout</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <ModalLogout></ModalLogout>
     </>
   );
 }
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Menu from "../components/Menu";
 
 function ProductsPage() {
   return (
     <>
       <Menu></Menu>
-      <div>ProductsPage</div>
+      <Container>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col xs={6}>2 of 3 (wider)</Col>
+          <Col>3 of 3</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col xs={5}>2 of 3 (wider)</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
     </>
   );
 }
