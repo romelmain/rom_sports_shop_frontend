18e48ba150486e3eddc944fd9f55f4a0b32981a9
romelmain <romelherreratgk@gmail.com>
2024-12-04 22:24:03 -0400

    Se agrego icono para indicar si hay o no productos en el carrito

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,6 +9,9 @@ import ModalLogout from "./modalLogout";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { ModalLogoutContext } from "../contexts/modallogout.context";
+import empty_cart from "../assets/empty-react-cart.png";
+import full_cart from "../assets/full-react-cart.png";
+import full_cart2 from "../assets/full-react-cart2.png";
 
 function Menu() {
   const { show, setShow, setModal } = useContext(ModalLogoutContext);
@@ -19,6 +22,9 @@ function Menu() {
         <Container>
           <Navbar.Brand href="/">Rom Sport Shop</Navbar.Brand>
           <Nav>
+            <Nav.Link href="#">
+              <img width="35px" height="35px" src={empty_cart}></img>
+            </Nav.Link>
             <Nav.Link href="/products">Products</Nav.Link>
             <Nav.Link href="/settings">Settings</Nav.Link>
             <Nav.Link onClick={setModal}>Logout</Nav.Link>
